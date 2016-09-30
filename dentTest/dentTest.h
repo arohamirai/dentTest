866b4fb61049c5f0d2b4e184e80f02232b942f43
HolaMirai <holamirai@163.com>
2016-09-30 17:32:56 +0800

    2016年9月30日17:32:54

---
+++ dentTest/dentTest.h
@@ -0,0 +1,32 @@
+
+// dentTest.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CdentTestApp:
+// 有关此类的实现，请参阅 dentTest.cpp
+//
+
+class CdentTestApp : public CWinApp
+{
+public:
+	CdentTestApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CdentTestApp theApp;
